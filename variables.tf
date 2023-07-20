32d85b6bd96c7a9f934d2f57c07ffb2928e6534f
Kjetil Hvarnes <kjetil@hvarnes.no>
2023-07-20 18:04:18 +0200

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -12,3 +12,15 @@ variable "name" {
   description = "Your name to attach to the webapp address."
   type        = string
 }
+
+variable "AWS_ACCESS_KEY_ID" {
+  description = ""
+  type        = string
+  sensitive   = true    
+}
+
+variable "AWS_SECRET_ACCESS_KEY" {
+  description = ""
+  type        = string
+  sensitive   = true 
+}
